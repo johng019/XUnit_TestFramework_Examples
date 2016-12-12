2bb47c40cbd35f5d0d7c417ed4c361a6115d531c
Gary <gljfb@yahoo.com>
2016-12-12 15:52:49 -0500

    Revision 12/12/16

--- XUnitCalcClass.cs
+++ XUnitCalcClass.cs
@@ -6,14 +6,14 @@ namespace XUnitTestExample.cs
     public class XUnitCalcClass
     {
         [Fact]
-        public void AddMethod()
+        public void VerifyAddMethodReturnsSumofTwoNumbers()
         {
             CalculatorClass obj1 = new CalculatorClass();
             double result = obj1.Add(4, 5);
             Assert.Equal(9, result);
         }
         [Fact]
-        public void TestSubtractMethodMultipleParameters()
+        public void VerifySubtractMethodReturnsDiffrenceOfTwoNumbers()
         {
             CalculatorClass obj2 = new CalculatorClass();
             double result = obj2.Subtract(10,5);
@@ -21,7 +21,7 @@ namespace XUnitTestExample.cs
         }
 
         [Fact]
-        public void TestMultiplyMethodMultipleParameters()
+        public void VerrifyMultiplyMethodReturnsProductOfTwoNumbers()
         {
             CalculatorClass obj3 = new CalculatorClass();
             double result = obj3.Multiply(3,9);
@@ -33,7 +33,7 @@ namespace XUnitTestExample.cs
         [InlineData(4, 2, 2)]
         [InlineData(8, 2, 4)]
         [InlineData(72, 9, 8)]
-        public void TestDivideMethodMultipleParameters(double num1, double num2, double expected)
+        public void VerifyDivideMethodUsingMultipleParameters(double num1, double num2, double expected)
         {
             CalculatorClass obj4 = new CalculatorClass();
             double result = obj4.Divide(num1, num2);
@@ -44,7 +44,7 @@ namespace XUnitTestExample.cs
         [InlineData(4, -5, -1)]
         [InlineData(-8, 2, -6)]
         [InlineData(-2, -9, -11)]
-        public void TestNegativeNumbersMultipleParameters(double num1, double num2,double expected)
+        public void VerifyUsingNegativeNumbersWithMultipleParametersReturnsCorrectValue(double num1, double num2,double expected)
         {
             CalculatorClass obj5 = new CalculatorClass();
             double result = obj5.Add(num1, num2);
@@ -53,15 +53,15 @@ namespace XUnitTestExample.cs
 
         //External method for geting multiple parameters
         [Theory]
-        [MemberData("ExternalParametersMethod")]
-        public void TestNonIntegersMultipleParameters(double num1, double num2, double expected)
+        [MemberData("SupplyingExternalParametersMethod")]
+        public void VerifyMultipleParametersFromExternalMethodProducesCorrectSum(double num1, double num2, double expected)
         {
             CalculatorClass obj6 = new CalculatorClass();
             double result = obj6.Add(num1, num2);
             Assert.Equal(expected, result);
         }
 
-        public static IEnumerable<object> ExternalParametersMethod()
+        public static IEnumerable<object> SupplyingExternalParametersMethod()
         {
             for (int i = 1; i < 5; i++)
             {
